4d6b99b7879065e670c5461ec3f6ca285243d4b7
Josue <josgar_14@hotmail.com>
2019-12-18 20:08:12 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+interface Product {
+	id: string;
+	title: string;
+	price: number;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'httpClient';
+	products$: Observable<Product[]>;
+
+	constructor(private httpClient: HttpClient) {
+		let httpHeaders = new HttpHeaders()
+			.set('Content-Type', 'application/json')
+			.set('Authorization', 'Basic QWxhZGRpb');
+
+		let httpParams = new HttpParams().set('title', 'First');
+
+		this.products$ = this.httpClient.get<Product[]>('/data/products.json', {
+			headers: httpHeaders,
+			params: httpParams
+		});
+		console.log(this.products$);
+	}
 }
